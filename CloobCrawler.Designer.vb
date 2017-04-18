1f815b6071b16327b606cf10133723bea915b4fa
Kmahmoudi <github@kmahmoudi.ir>
2017-04-19 00:00:33 +0430

    Add files via upload

---
+++ CloobCrawler.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CloobCrawler
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DownloadTXT = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.URLTextBox = New System.Windows.Forms.TextBox()
+        Me.CloobIDTextBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.statusLbl = New System.Windows.Forms.Label()
+        Me.Browser = New System.Windows.Forms.WebBrowser()
+        Me.OutputTextBox = New System.Windows.Forms.TextBox()
+        Me.ZzTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.DownloadTXT)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.URLTextBox)
+        Me.GroupBox1.Controls.Add(Me.CloobIDTextBox)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.statusLbl)
+        Me.GroupBox1.Location = New System.Drawing.Point(5, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(582, 112)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Download: "
+        '
+        'DownloadTXT
+        '
+        Me.DownloadTXT.Location = New System.Drawing.Point(75, 19)
+        Me.DownloadTXT.Name = "DownloadTXT"
+        Me.DownloadTXT.Size = New System.Drawing.Size(100, 20)
+        Me.DownloadTXT.TabIndex = 5
+        Me.DownloadTXT.Text = "false"
+        '
+        'Button4
+        '
+        Me.Button4.ForeColor = System.Drawing.Color.Black
+        Me.Button4.Location = New System.Drawing.Point(434, 27)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "End"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(353, 27)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Extract Info"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(272, 27)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Crawl"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'URLTextBox
+        '
+        Me.URLTextBox.Location = New System.Drawing.Point(75, 75)
+        Me.URLTextBox.Name = "URLTextBox"
+        Me.URLTextBox.Size = New System.Drawing.Size(501, 20)
+        Me.URLTextBox.TabIndex = 2
+        '
+        'CloobIDTextBox
+        '
+        Me.CloobIDTextBox.Location = New System.Drawing.Point(75, 49)
+        Me.CloobIDTextBox.Name = "CloobIDTextBox"
+        Me.CloobIDTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.CloobIDTextBox.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(191, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "URL : "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CloobID : "
+        '
+        'statusLbl
+        '
+        Me.statusLbl.AutoSize = True
+        Me.statusLbl.Location = New System.Drawing.Point(493, 27)
+        Me.statusLbl.Name = "statusLbl"
+        Me.statusLbl.Size = New System.Drawing.Size(0, 13)
+        Me.statusLbl.TabIndex = 0
+        '
+        'Browser
+        '
+        Me.Browser.Location = New System.Drawing.Point(5, 121)
+        Me.Browser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.Browser.Name = "Browser"
+        Me.Browser.Size = New System.Drawing.Size(582, 250)
+        Me.Browser.TabIndex = 1
+        Me.Browser.Visible = False
+        '
+        'OutputTextBox
+        '
+        Me.OutputTextBox.BackColor = System.Drawing.Color.Black
+        Me.OutputTextBox.ForeColor = System.Drawing.Color.Aqua
+        Me.OutputTextBox.Location = New System.Drawing.Point(5, 121)
+        Me.OutputTextBox.Multiline = True
+        Me.OutputTextBox.Name = "OutputTextBox"
+        Me.OutputTextBox.Size = New System.Drawing.Size(582, 250)
+        Me.OutputTextBox.TabIndex = 2
+        '
+        'ZzTimer
+        '
+        Me.ZzTimer.Interval = 1000
+        '
+        'CloobCrawler
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(595, 383)
+        Me.Controls.Add(Me.OutputTextBox)
+        Me.Controls.Add(Me.Browser)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.ForeColor = System.Drawing.Color.Lime
+        Me.Name = "CloobCrawler"
+        Me.Text = "Cloob Crawler"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CloobIDTextBox As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents statusLbl As Label
+    Friend WithEvents URLTextBox As TextBox
+    Friend WithEvents Browser As WebBrowser
+    Friend WithEvents OutputTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ZzTimer As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DownloadTXT As TextBox
+End Class
---
+++ CloobCrawler.vb
@@ -0,0 +1,165 @@
+﻿Public Class CloobCrawler
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GraphDb.CreateNode("USER", "CloobID", CloobIDTextBox.Text.ToString)
+        GraphDb.SetProperty("USER", "CloobID", CloobIDTextBox.Text.ToString, "URL", URLTextBox.Text.ToString)
+        GraphDb.SetProperty("USER", "CloobID", CloobIDTextBox.Text.ToString, "FriendsIndexed", "false")
+        MsgBox("User Added !")
+        CloobIDTextBox.Clear()
+        URLTextBox.Clear()
+    End Sub
+
+    Private Sub CloobCrawler_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Browser.ScriptErrorsSuppressed = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Crawl()
+        statusLbl.Text = ""
+    End Sub
+
+    Public Sub FindFriends(ByVal node As String)
+        Dim url As String = "http://www.cloob.com/profile/friend/list/username/" + node + "/wrapper/true"
+        'Dim Browser As New WebBrowser
+        Browser.Navigate(url)
+        URLTextBox.Text = url
+        While (Browser.ReadyState <> WebBrowserReadyState.Complete)
+            My.Application.DoEvents()
+        End While
+        GraphDb.SetProperty("USER", "CloobID", node, "FriendsIndexed", "true")
+        'scroll to end
+        Dim lastGuy = Browser.Document.GetElementById("lastElement")
+        If lastGuy <> Nothing Then
+            lastGuy.ScrollIntoView(True)
+        End If
+        For Each lnk As HtmlElement In Browser.Document.Links
+            If lnk.GetAttribute("href").ToString.ToLower.Contains("/name/") Then
+                Dim cloobID As String
+                cloobID = lnk.GetAttribute("href")
+                cloobID = cloobID.Substring(cloobID.IndexOf("/name/") + 6)
+                cloobID = cloobID.Substring(0, cloobID.IndexOf("/"))
+                If (GraphDb.GetNodeProperty("USER", "URL", "CloobID", cloobID) = "null") Then
+                    GraphDb.CreateNode("USER", "CloobID", cloobID)
+                    GraphDb.SetProperty("USER", "CloobID", cloobID, "URL", lnk.GetAttribute("href"))
+                    GraphDb.SetProperty("USER", "CloobID", cloobID, "FriendsIndexed", "false")
+                    GraphDb.SetProperty("USER", "CloobID", cloobID, "HasProfileInfo", "false")
+
+                End If
+                If (node <> cloobID) Then
+                    Relation("FriendsWith", "USER", "USER", "CloobID", node, "CloobID", cloobID)
+                    OutputTextBox.AppendText(cloobID + " is friends with " + node + vbNewLine)
+                End If
+            End If
+        Next
+    End Sub
+
+
+    Public Sub ProfileInfo(ByVal node As String)
+        On Error Resume Next
+        Dim repo As String = "D:\CloobRepo\"
+        Dim url As String = "http://www.cloob.com/profile/" + node
+        'Dim Browser As New WebBrowser
+        Browser.Navigate(url)
+        URLTextBox.Text = url
+        While (Browser.ReadyState <> WebBrowserReadyState.Complete)
+            My.Application.DoEvents()
+        End While
+        'scroll to end
+        If DownloadTXT.Text = "true" Then
+            For Each img As HtmlElement In Browser.Document.Images
+                If img.GetAttribute("src").Contains("/public/user_data/user_photo/") Then
+                    GraphDb.SetProperty("USER", "CloobID", node, "ProfilePic", img.GetAttribute("src"))
+                    My.Computer.Network.DownloadFile(img.GetAttribute("src"), repo + node + ".jpg")
+                    While (My.Computer.FileSystem.FileExists(repo + node + ".jpg") = False)
+                        My.Application.DoEvents()
+                    End While
+                    Exit For
+                Else
+                    GraphDb.SetProperty("USER", "CloobID", node, "ProfilePic", "")
+                End If
+            Next
+        End If
+        Dim Data As String = Browser.Document.GetElementById("userProfile_box_main").InnerText
+        GraphDb.SetProperty("USER", "CloobID", node, "Name", subStringer("نام", "شغل", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Job", subStringer("شغل", "درباره من", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "AboutMe", subStringer("درباره من", "وضعیت", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Status", subStringer("وضعیت", "محل سکونت", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Location", subStringer("محل سکونت", "تحصيلات", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "WebSite", subStringer("صفحه وب", "تاریخ عضویت", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "MemberDate", subStringer("تاریخ عضویت", "", Data))
+        Data = Browser.Document.GetElementById("userProfile_box_interest").InnerText
+        GraphDb.SetProperty("USER", "CloobID", node, "Favorite", subStringer("علایقعلایق", "ورزش", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Sports", subStringer("ورزش", "فعاليتها", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Activity", subStringer("فعاليتها", "کتاب", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Books", subStringer("کتاب", "موسيقي", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Musics", subStringer("موسيقي", "برنامه تلويزيون", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Tv", subStringer("برنامه تلويزيون", "فيلمها", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Movie", subStringer("فيلمها", "غذا", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Food", subStringer("غذا", "", Data))
+        Data = Browser.Document.GetElementById("userProfile_box_ability").InnerText
+        GraphDb.SetProperty("USER", "CloobID", node, "University", subStringer("دانشگاه", "دبيرستان", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "HighSchool", subStringer("دبيرستان", "رشته", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "Field", subStringer("رشته", "سال فارغ التحصيلي", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "GradYear", subStringer("سال فارغ التحصيلي", "نام محل کار", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "JobTitle", subStringer("عنوان شغلي", "در مورد کار من", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "AboutMyJob", subStringer("در مورد کار من", "ايميل کاري", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "JobEmail", subStringer("ايميل کاري", "وب سايت کاري", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "WorkPhone", subStringer("شماره تلفن کار", "مهارتها", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "WorkFav", subStringer("علايق کاري", "", Data))
+        GraphDb.SetProperty("USER", "CloobID", node, "HasProfileInfo", "true")
+
+    End Sub
+    Public Sub Crawl()
+        ZzTimer.Enabled = False
+        Dim node As String = ""
+        While (1)
+            node = GraphDb.GetNodeProperty("USER", "CloobID", "FriendsIndexed", "false")
+            If (node <> "null") Then
+                My.Application.DoEvents()
+                FindFriends(node)
+                ProfileInfo(node)
+                OutputTextBox.AppendText("Listing Friends Of " + node + vbNewLine)
+            Else
+                Exit While
+            End If
+        End While
+        ZzTimer.Enabled = True
+    End Sub
+    Private Sub CloobCrawler_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        ' Crawl()
+    End Sub
+
+    Private Sub ZzTimer_Tick(sender As Object, e As EventArgs) Handles ZzTimer.Tick
+        If (statusLbl.Text = "") Then
+            statusLbl.Text = "System Idle"
+        Else
+            statusLbl.Text = ""
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ProfileInfo(CloobIDTextBox.Text)
+    End Sub
+    Public Function subStringer(ByVal Str1 As String, ByVal Str2 As String, ByVal input As String) As String
+        Dim result As String = ""
+        If Str2 <> "" Then
+            Dim x1 = input.IndexOf(Str1)
+            Dim x2 = input.IndexOf(Str2)
+            If (x1 = -1 Or x2 = -1) Then
+                Return ""
+            End If
+            result = input.Substring(x1 + Str1.Length, x2 - x1 - Str1.Length)
+        Else
+            Dim x1 = input.IndexOf(Str1)
+            If (x1 = -1) Then
+                Return ""
+            End If
+            result = input.Substring(x1 + Str1.Length)
+        End If
+        Return result
+    End Function
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
+End Class
